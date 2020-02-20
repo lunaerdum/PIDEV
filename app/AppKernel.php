dbf44ea737ec3651ab205e5ddd7a4e43204b47a2
ArbiaBougamra <arbia.bougamra@esprit.tn>
2020-02-20 14:26:25 +0100

    Added Employer Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new EmpoloyerBundle\EmpoloyerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
